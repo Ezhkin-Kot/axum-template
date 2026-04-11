83b2bba4c30880d6b717d24a363af9b1d26cd606
aragami3070 <smirnov17612@gmail.com>
2026-04-11 22:06:55 +0400

    feat(config): add token repo and token service

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,9 @@ use sqlx::postgres::PgPoolOptions;
 use std::env;
 use std::sync::Arc;
 
+use crate::repository::tokens::TokenRepo;
 use crate::repository::users::UserRepo;
+use crate::services::auth::tokens::TokenService;
 
 pub struct Config {
     pub database_url: String,
@@ -28,9 +30,16 @@ pub async fn get_db_pool(database_url: &str) -> sqlx::PgPool {
 
 #[derive(Clone)]
 pub struct AppState {
+    // NOTE: репозитории
     pub user_repo: Arc<UserRepo<Postgres>>,
-    pub secret_key: String,
-    pub secret_refresh_key: String,
+    pub token_repo: Arc<TokenRepo<Postgres>>,
+
+    // NOTE: сервисы
+    pub token_serv: Arc<TokenService<Postgres>>,
+
+    // NOTE: Ключи и тп
+    pub secret_key: Arc<String>,
+    pub secret_refresh_key: Arc<String>,
 }
 
 impl AppState {
@@ -39,8 +48,22 @@ impl AppState {
         secret_key: String,
         secret_refresh_key: String,
     ) -> Self {
+        let user_repo = Arc::new(UserRepo::new(db_pool.clone()));
+        let token_repo = Arc::new(TokenRepo::new(db_pool.clone()));
+
+        let secret_key = Arc::new(secret_key);
+        let secret_refresh_key = Arc::new(secret_refresh_key);
         Self {
-            user_repo: Arc::new(UserRepo::new(db_pool.clone())),
+            token_serv: Arc::new(TokenService::new(
+                secret_key.clone(),
+                secret_refresh_key.clone(),
+                5,
+                token_repo.clone(),
+                user_repo.clone(),
+                1440,
+            )),
+            user_repo,
+            token_repo,
             secret_key,
             secret_refresh_key,
         }
