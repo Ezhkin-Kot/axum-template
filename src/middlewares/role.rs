b883a40695da42174321cba4eee91aaaaea1700e
aragami3070 <smirnov17612@gmail.com>
2026-04-12 21:56:03 +0400

    feat(role-middleware): add validate role in RoleGuardMiddleware

--- src/middlewares/role.rs
+++ src/middlewares/role.rs
@@ -26,3 +26,35 @@ pub struct RoleGuardMiddleware<S> {
     inner: S,
     allowed_roles: Vec<Role>,
 }
+
+impl<S, ReqBody> Service<Request<ReqBody>> for RoleGuardMiddleware<S>
+where
+    S: Service<Request<ReqBody>, Error = axum::response::Response> + Clone + Send + 'static,
+    S::Future: Send + 'static,
+    ReqBody: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = axum::response::Response;
+    type Future =
+        futures::future::BoxFuture<'static, Result<Self::Response, axum::response::Response>>;
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), axum::response::Response>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
+        let mut inner = self.inner.clone();
+        let claims = req.extensions().get::<Claims>().cloned();
+        let allowed_roles = self.allowed_roles.clone();
+        Box::pin(async move {
+            let claims = match claims {
+                Some(cl) => cl,
+                None => return Err(AuthError::Unauthorized.into_response()),
+            };
+
+            if !allowed_roles.contains(&Role::from(claims.role)) {
+                return Err(AuthError::Forbidden.into_response());
+            }
+            inner.call(req).await
+        })
+    }
+}
