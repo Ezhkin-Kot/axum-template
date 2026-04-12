a6c086b56ba8a09dfdaaaa49fae83c3d531fbbfe
aragami3070 <smirnov17612@gmail.com>
2026-04-12 21:50:14 +0400

    feat(token-error): add InvalidAuthHeader and InvalidAuthSchema

--- src/errors/tokens.rs
+++ src/errors/tokens.rs
@@ -7,14 +7,25 @@ use http::StatusCode;
 pub enum TokenError {
     #[error("Database error: {0}")]
     Database(#[from] sqlx::Error),
+
     #[error("JWT error: {0}")]
     Jwt(#[from] jsonwebtoken::errors::Error),
+
     #[error("SystemTime error: {0}")]
     DataTime(#[from] SystemTimeError),
+
     #[error("Token expired")]
     Expired,
+
     #[error("Invalid token")]
     InvalidToken,
+
+    #[error("Invalid auth header")]
+    InvalidAuthHeader,
+
+    #[error("Invalid auth schema")]
+    InvalidAuthSchema,
+
     #[error("Refresh token not found in database")]
     RefreshNotFound,
 }
@@ -30,6 +41,12 @@ impl IntoResponse for TokenError {
                 "Refresh token expired".to_string(),
             ),
             Self::InvalidToken => (StatusCode::UNAUTHORIZED, "Invalid token".to_string()),
+            Self::InvalidAuthHeader => {
+                (StatusCode::UNAUTHORIZED, "Invalid auth header".to_string())
+            }
+            Self::InvalidAuthSchema => {
+                (StatusCode::UNAUTHORIZED, "Invalid auth schema".to_string())
+            }
             Self::RefreshNotFound => (
                 StatusCode::UNAUTHORIZED,
                 "Refresh token not found".to_string(),
